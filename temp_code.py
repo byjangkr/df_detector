2f1852d75d2b6fe18b27655a4f41347dbb009f4c
byjangkr <darkbulls44@gmail.com>
2016-12-21 17:37:17 +0900

    create run file for script

--- temp_code.py
+++ temp_code.py
@@ -4,8 +4,14 @@ import argparse
 from func import dfio
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("dsf_file",help="input dsf file name")
-args=parser.parse_args()
+#parser = argparse.ArgumentParser()
+#parser.add_argument("dsf_file",help="input dsf file name")
+#args=parser.parse_args()
+dsf_file = 'dsf/F_1_KSM_03_r2.dsf'
 
-dfio.plotprorepimg(args.dsf_file)
+#dfio.plotprorepimg(dsf_file)
+
+rep_margin = 20 # rep_margin*10ms
+pro_img, rep_img = dfio.loadprorepimg(dsf_file)
+
+
