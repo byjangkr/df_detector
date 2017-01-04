e6579ab9d3c6b7133ec0615b12530fbb9ce09eeb
byjangkr <darkbulls44@gmail.com>
2017-01-04 09:11:49 +0900

    fix_scp_for_est

--- scp_ext_dsf_feat.sh
+++ scp_ext_dsf_feat.sh
@@ -9,9 +9,26 @@ out_file=$2
 [ -z "$out_file" ] && echo "error!! need to output_file" && exit 1;
 
 cd df_detector
-find $dsf_img_dir -iname "*.dsf" > $dsf_img_dir/dsfimglist.log
+find $dsf_img_dir -iname "*.dsf_img" > $dsf_img_dir/dsfimglist.log
 
 touch $out_file
+
+# output for EST 
+# speaker set task feat1 feat2 feat3 ...
+while read line
+do
+  dsf_img_file=$line
+  speaker=$(echo $line | sed 's#.*/[FM]*_[0-9]*_\([A-Z]*\)_[0-9]*\.dsf_img#\1#g')
+  setnum=$(echo $line | sed 's#.*/[FM]*_\([0-9]*\)_[A-Z]*_[0-9]*\.dsf_img#\1#g')
+  tasknum=$(echo $line | sed 's#.*/[FM]*_[0-9]*_[A-Z]*_\([0-9]*\)\.dsf_img#\1#g')
+  echo -n "$speaker $setnum $tasknum " >> $out_file
+  ext_dsffeat_dsfimg.py $dsf_img_file >> $out_file
+
+done < $dsf_img_dir/dsfimglist.log
+
+# general output
+# filename feat1 feat2 feat3 ...
+: << 'General'
 while read line
 do
   dsf_img_file=$line
@@ -21,6 +38,7 @@ do
   ext_dsffeat_dsfimg.py $dsf_img_file >> $out_file
 
 done < $dsf_img_dir/dsfimglist.log
+General
 
 : << 'END'
 usage: ext_dsffeat_dsfimg.py [-h] [-m OUTMODE] [-o OUTFILE] dsf_img_file
